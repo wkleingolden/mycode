dc85f0aa64a3fc111f934e0627d2d516b94ff28b
William Klein <wkleingolden@users.noreply.github.com>
2022-08-22 18:13:21 +0000

    Learning about go basics.

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,14 @@
+/* Lab 11 Challenge 01 - WKlein 9/22/22 */
+
+package main
+  
+import "fmt"
+func  main() {
+var fullname, citystate  = "Bill Klein", "Golden, Colorado"
+
+    fmt.Println("My name is:", fullname) 
+    fmt.Println("I live in:", citystate)
+    
+
+}
+
