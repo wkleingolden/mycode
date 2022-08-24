8df71da14cd0ff5958b8620e750798eb102cf9c4
William Klein <wkleingolden@users.noreply.github.com>
2022-08-24 16:47:32 +0000

    working with defer

---
+++ defer01.go
@@ -0,0 +1,56 @@
+/* RZFeeser | Alta3 Research
+   Defer - cleanup operations on file creation  */
+   
+package main
+
+import (
+    "fmt"
+    "os"
+    "os/exec"
+)
+
+// expects input, "p" to be a string
+// will return os.File object
+func createFile(p string) *os.File {
+    fmt.Println("Creating file...")
+    f, err := os.Create(p)
+    if err != nil {
+        panic(err)
+    }
+    return f
+}
+
+// exects input, f, to be an os.File object
+// returns nothing
+func writeFile(f *os.File) {
+    fmt.Println("Writing employee list to file...")
+    fmt.Fprintln(f, "Cloud Strife\nAerith Gainsborough\nRed XIII\nVincent Valentine")
+
+}
+
+func closeFile(f *os.File) {
+    fmt.Println("Closing file...")
+    err := f.Close()
+
+    // best practice says to still check for errors
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "error: %v\n", err)
+        os.Exit(1)
+    }
+}
+
+func main() {
+
+    cmd := exec.Command("clear") // Clear the term screen before program run
+    cmd.Stdout = os.Stdout
+    cmd.Run()
+
+    // create file object 
+    f := createFile("/tmp/employeeList.txt")
+
+    // after getting back a file object, we defer closing closeFile()
+    defer closeFile(f)   // closeFile() will be executed at end of the enclosing
+                         // function-- in this case, main()
+    writeFile(f)
+}
+
