2fbc6c45f8e70c7dfca974f8add3f5873d158084
William Klein <wkleingolden@users.noreply.github.com>
2022-08-25 01:56:31 +0000

    working with json files

---
+++ jsonfiles01.go
@@ -0,0 +1,69 @@
+/* RZFeeser | Alta3 Research
+Reading JSON - with structs  */
+
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "os"
+    "strconv"
+)
+
+// Users is a slice that containers
+// an indefinite number of the User struct
+type Users struct {
+    Users []User `json:"users"`
+}
+
+// User struct which contains a name
+// a type and a list of social links
+type User struct {
+    Name   string `json:"name"`
+    Type   string `json:"type"`
+    Age    int    `json:"Age"`
+    Social Social `json:"social"`
+}
+
+// Social struct which contains a
+// list of links
+type Social struct {
+    Facebook string `json:"facebook"`
+    Twitter  string `json:"twitter"`
+}
+
+func main() {
+    // Open our jsonFile
+    jsonFile, err := os.Open("users.json")
+    // if we os.Open returns an error then handle it
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("Successfully Opened users.json")
+    // defer the closing of our jsonFile so that we can parse it later on
+    defer jsonFile.Close()
+
+    // read our opened file as a byte array.
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+
+    // we initialize our Users struct
+    // this will contain an indefinite number of user types
+    var users Users
+
+    // we unmarshal our byteArray which contains our
+    // jsonFile's content into 'users' which we defined above
+    json.Unmarshal(byteValue, &users)
+
+    // we iterate through every user within our users slice and
+    // print out the user Type, their name, and their facebook url
+    // as just an example
+    for i := 0; i < len(users.Users); i++ {
+        fmt.Println("User Type: " + users.Users[i].Type)
+        fmt.Println("User Age: " + strconv.Itoa(users.Users[i].Age))
+        fmt.Println("User Name: " + users.Users[i].Name)
+        fmt.Println("Facebook Url: " + users.Users[i].Social.Facebook)
+    }
+
+}
---
+++ jsonfiles02.go
@@ -0,0 +1,43 @@
+/* RZFeeser | Alta3 Research
+Writing out JSON              */
+
+package main
+
+import (
+    "encoding/json"
+    "os"
+)
+
+func main() {
+    type Person struct {
+        Fn string
+        Ln string
+    }
+    type ColorGroup struct {
+        ID     int
+        Name   string
+        Colors []string
+        P      Person `json:"Person"`
+    }
+
+    per := Person{Fn: "RZ",
+        Ln: "Feeser",
+    }
+
+    group := ColorGroup{
+        ID:     24601,
+        Name:   "Greens",
+        Colors: []string{"Moss", "Shamrock", "Lime", "Hunter"},
+        P:      per,
+    }
+
+    // serialize values from struct to json format
+    b, err := json.Marshal(group)
+
+    if err != nil {
+        panic(err)
+    }
+
+    // print to standard out
+    os.Stdout.Write(b)
+}
